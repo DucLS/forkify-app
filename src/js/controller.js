0a782d8cf8735d775ac0281908d3046227d7b4fb
le.sy.duc <SUN-ASTERISK\le.sy.duc@b122223-pc.sun-asterisk.com>
2022-10-06 16:47:40 +0700

    Implement MVC pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,31 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from "./model";
+import recipeView from "./views/recipeView";
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
+    // Before load recipe
+    recipeView.renderSpinner();
+
+    // Loading recipe
+    await model.loadRecipe(id);
+
+    // Render recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+["hashChange", "load"].forEach((event) =>
+  window.addEventListener(event, showRecipe)
+);
---
+++ src/js/model.js
@@ -0,0 +1,27 @@
+import { async } from "regenerator-runtime";
+import { API_URL } from "./config";
+import { getJson } from "./helpers";
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJson(`${API_URL}/${id}`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.soure_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    console.log(err.message);
+  }
+};
